fbc19dfacec6f0259ca8bb939bc32a6e821b2cf0
Ryan O'Boyle <wronghandeddev@gmail.com>
2020-08-19 05:41:24 -0700

    whatever

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+/* jshint ignore:start*/
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
 import './App.css';
-import Table from './components/Table'
+import Table from './components/Table';
 
 function App() {
   const [users, setUsers] = useState([]);
@@ -9,25 +10,25 @@ function App() {
   useEffect(()=>{
     axios.get("https://randomuser.me/api/?results=200&nat=us").then(({data})=>{
       setUsers(data.results);
-      setFilteredUsers(data.results)
-    })
-  },[])
+      setFilteredUsers(data.results);
+    });
+  },[]);
 
   const handleFilter = input => {
     //do some filtering on the users state, and change it
-    const filtered = users.filter(item=> item.name.first.toLowerCase().includes(input) || item.name.last.toLowerCase().includes(input) || item.email.includes(input) )
-    setFilteredUsers(filtered)
-  }
+    const filtered = users.filter(item=> item.name.first.toLowerCase().includes(input) || item.name.last.toLowerCase().includes(input) || item.email.includes(input) );
+    setFilteredUsers(filtered);
+  };
 
   return <>
     <div className="jumbotron">
       <h1 style={{textAlign:"center"}}>Employee Directory</h1>
     </div>
     <div className="container">
-      <input onChange={(e)=> handleFilter(e.target.value.toLowerCase())} style={{width:"80%",marginLeft:"10%"}} placeholder="Type in a name to search the directory"/>
+      <input onChange={(event)=> handleFilter(event.target.value.toLowerCase())} style={{width:"60%",marginLeft:"30%"}} placeholder="Look up Employee By Name"/>
       <Table users={filteredUsers}/>
     </div>
   </>
-}
+};
 
 export default App;
